82607cbe9c2cec9d47c8cb00572d40965ac5c047
a-hashimoto <a-hashimoto@unext.jp>
2019-01-24 21:47:35 +0900

    reset

---
+++ code_template/convinience_tool.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <functional>
+
+
+typedef long long ll;
+#define FOR(i, a, b) for(int i = (a); i < (b); i++)
+#define REP(i, n) for(int i = 0; i < n; i++)
+#define define pb push_back
+#define ALL(obj) (obj).begin(), (obj).end()
+#define int long long
